bd67ad14890eb42c0198e172009139454e25624a
smritig26 <smritigrover2003@gmail.com>
2024-08-21 00:04:56 +0530

    adding products js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-// console.log('hello')
-const products = [
-    {
-        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name :  'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        ratings : {
-            stars : 4.5,
-            count : 87
-        },
-        pricecents : 1090
-    },
-    {
-       image : 'images/products/intermediate-composite-basketball.jpg',
-       name : 'Intermediate Size Basketball',
-       ratings : {
-        stars : 4,
-        count : 127,
-       },
-       pricecents : 2095 
-    },
-    {
-        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-       name : ' Adults Plain Cotton T-Shirt - 2 Pack',
-       ratings : {
-        stars : 4.5,
-        count : 56,
-       },
-       pricecents : 799
-    }
-];
+
 
 let productshtml = '';
 products.forEach((product) => {
