302db55b1486cc204ec450e554a60851502c3e5b
smritig26 <smritigrover2003@gmail.com>
2024-08-23 17:31:09 +0530

    delete products from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     });
     // console.log(matchingProduct);
     cartSummaryHTML += 
-`<div class="cart-item-container">
+`<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,7 +100,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         const productId = link.dataset.productId;
         // console.log(productId);
         removeFromCart(productId);
-        console.log(cart);
+        // console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        console.log(container);
+        container.remove();
     });
 });
 
