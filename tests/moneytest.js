7b231f269957b2c0ca3ec240af1d40d384944731
smritig26 <smritigrover2003@gmail.com>
2024-09-05 21:21:23 +0530

    create automated tests for formatcurrency

---
+++ tests/moneytest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if(formatCurrency(2095) === "20.95"){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+if(formatCurrency(0) == '0.00'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
