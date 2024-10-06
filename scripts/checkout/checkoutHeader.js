3b554192c463fc318b8cc130f96f0b8be62b6dd8
smritig26 <smritigrover2003@gmail.com>
2024-10-06 14:08:11 +0530

    chnages

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -4,7 +4,7 @@ export function renderCheckoutHeader(){
     const checkoutHeaderHTML = `
     <div class="header-content">
         <div class="checkout-header-left-section">
-          <a href="amazon.html">
+          <a href="index.html">
             <img class="amazon-logo" src="images/amazon-logo.png">
             <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
           </a>
@@ -12,7 +12,7 @@ export function renderCheckoutHeader(){
 
         <div class="checkout-header-middle-section">
           Checkout (<a class="return-to-home-link js-header-update"
-            href="amazon.html">${q}</a>)
+            href="index.html">${q}</a>)
         </div>
 
         <div class="checkout-header-right-section">
